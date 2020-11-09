36310d024926bc1620fc96f56ae6da795d88ee85
firozmahmud <firozmahmud.cse@gmail.com>
2020-11-09 23:49:09 +0600

    2

--- app/src/main/java/com/example/my_pattern_in_android/di/AppModule.kt
+++ app/src/main/java/com/example/my_pattern_in_android/di/AppModule.kt
@@ -6,9 +6,12 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
+
 
 @Module
 @InstallIn(ApplicationComponent::class)
@@ -17,12 +20,20 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideRetrofit(): Retrofit = Retrofit.Builder().baseUrl(BuildConfig.BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create()).build()
+    fun provideRetrofit(client: OkHttpClient): Retrofit =
+        Retrofit.Builder().baseUrl(BuildConfig.BASE_URL).client(client)
+            .addConverterFactory(GsonConverterFactory.create()).build()
 
 
     @Provides
     @Singleton
     fun provideMyApi(retrofit: Retrofit): MyApi = retrofit.create(MyApi::class.java)
 
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient =
+        OkHttpClient.Builder().connectTimeout(10, TimeUnit.SECONDS)
+            .callTimeout(10, TimeUnit.SECONDS)
+            .readTimeout(10, TimeUnit.SECONDS)
+            .writeTimeout(10, TimeUnit.SECONDS).build()
 }
